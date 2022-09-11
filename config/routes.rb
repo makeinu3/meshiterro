258e0cc185f9739916b62aee004c2b643720496d
makeinu3 <112616049+makeinu3@users.noreply.github.com>
2022-09-11 07:15:17 +0000

    [Add] 4-9

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
+
   devise_for :users
   get "/homes/about" => "homes#about", as: "about"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  resources :postimages, only: [:new, :index, :show]
 
   root to: "homes#top"
 
